1cfd4dd43a41b99f3592b95499a70c9f4a2c99c3
AchieverGold <sundayadeyemo6029@gmail.com>
2023-02-20 03:13:57 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - 2-print_alphabet
+ *
+ * Return- always return 0
+ *
+ */
+ int main(void)
+{
+	char ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++) 
+	{
+		putchar(ch);
+	}
+	return 0;
+}
