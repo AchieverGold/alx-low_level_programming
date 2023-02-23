e90475ce58a2aadc885d3edac35e7fc5c8f67ab3
AchieverGold <sundayadeyemo6029@gmail.com>
2023-02-23 04:33:25 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - Draw a straight line according to the values
+ * @n: The number of line to draw
+ * Return: empty
+ */
+
+void print_line(int n)
+{
+int x;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+}
